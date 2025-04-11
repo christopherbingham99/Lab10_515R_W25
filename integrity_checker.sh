ed434df63764d9e3c282581153cc80c3b8df10ca
Chris <christopherbingham99@gmail.com>
2025-04-11 01:49:54 -0600

    if

--- integrity_checker.sh
+++ integrity_checker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "collecting hashes..."
+echo collecting hashes...
 
 FILE1=dont_modify_these/dont_change_me.txt
 HASH1=$(md5sum $FILE1 | cut -d ' ' -f 1)
-echo "collected" $FILE1 $HASH1
+echo collected $FILE1 $HASH1
 
-echo "done collecting hashes."
+echo done collecting hashes.
 
+echo scanning...
 
-
-echo "scanning..."
+if [ $FILE1 == $FILE1 ]; then
+  echo lol
+fi
