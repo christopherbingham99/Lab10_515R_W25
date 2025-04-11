0efd6413452e23a18e98a3a1e11145688b111a4b
Chris <christopherbingham99@gmail.com>
2025-04-11 07:13:02 +0000

    adced files

--- integrity_checker.py
+++ integrity_checker.py
@@ -1,1 +1,13 @@
-import os
+#!/bin/bash
+
+echo "collecting hashes..."
+
+FILE1=dont_modify_these/dont_change_me.txt
+HASH1=$(md5sum $FILE1 | cut -d ' ' -f 1)
+echo "collected" $FILE1 $HASH1
+
+echo "done collecting hashes."
+
+
+
+echo "scanning..."
